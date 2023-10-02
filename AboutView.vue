1dd790212ad02edd0d5da225e6a6a0213854a3a9
Labiba33 <142963823+Labiba33@users.noreply.github.com>
2023-10-02 13:32:32 +0600

    Add files via upload

---
+++ AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1></h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+}
+</style>
---
+++ maincard.vue
@@ -0,0 +1,68 @@
+<template>
+    
+    
+    
+    
+    
+    <div class="main-container">
+        <div class="profile">
+            <img src="Lt.jpg" alt="Lt logo">
+            <h3> Labiba Tanjim</h3>
+            <p> Bachelors of IT</p>
+        </div>
+            <div class="Contact">
+                <p>labibatanjimrida@gmail.com</p>
+            </div>
+        <div class="about me">
+            <p>
+                Hi, I am glad that you visited my website. I am a IT student at Deakin University. 
+                I am passionate about learning new things and meeting new people. I want to 
+                be a software developer in my near future. I have my resume as well as some of the certificates of my degree.
+            </p>        
+
+
+        </div>
+        <div>
+        
+            <div class="button container">
+               
+                <a href="https://drive.google.com/file/d/1FM1XdJzxk57gXTgexzFERPL46_HXVp9Z/view?usp=sharing" 
+                download=" my resume">My resume</a>
+
+
+                
+                <a href="https://drive.google.com/file/d/1CI2UX0NbHphTPlR6D6ET2SJzl8Qv1zIh/view?usp=sharing">My certificates</a>
+
+
+                <a href="https://drive.google.com/file/d/1F2Vc70IaUrnmPfWXr8jPyjDkL8cxOQXT/view?usp=sharing">My Experiences in this field</a>
+
+            </div>
+        </div>
+
+
+
+    </div>
+    <div class="container2">
+        <img scr="ty.jpg">
+
+    </div>
+
+
+
+
+</template>
+
+<style scoped>
+    img{
+        height: 200px;
+    }
+
+    .main-container{
+        background-color:rgb(241, 214, 182);
+
+    }
+    img{
+        height:150px
+    }
+
+</style>
